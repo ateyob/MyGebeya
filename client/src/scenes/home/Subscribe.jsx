90de818eb0d9b769f9024e99cff8daf281099bf3
Eyob Kefyalew <ateyob@gmail.com>
2023-02-01 18:27:10 +0300

    Footer + subscription

---
+++ client/src/scenes/home/Subscribe.jsx
@@ -0,0 +1,49 @@
+import { MarkEmailReadOutlined } from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  Input,
+  InputBase,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { useState } from "react";
+
+const Subscribe = () => {
+  const [email, setEmail] = useState("");
+
+  return (
+    <Box width="80%" margin="80px auto" textAlign="center">
+      <IconButton>
+        <MarkEmailReadOutlined fontSize="large" />
+      </IconButton>
+
+      <Typography variant="h3">Subscribe To Our NewsLetter</Typography>
+      <Typography>
+        and recieve 50 Birr Coupon for your first order when you checkout
+      </Typography>
+      <Box
+        p="2px 4px"
+        m="15px auto"
+        display="flex"
+        alignItems="center"
+        width="75%"
+        backgroundColor="#F2F2F2"
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <Typography sx={{ p: "10px", ":hover": { cursor: "pointer" } }}>
+          Subscribe
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Subscribe;
